772b545643321dea70896ce69ed8a416ffed065e
RyanDwitama <76994573+RyanDwitama@users.noreply.github.com>
2021-01-22 09:36:02 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,15 +8,15 @@ public class Main {
 
     static void solution(int N) {
         int enablePrint = 0;
-        int zero = 0;
+        int zero = 0; //inisialisasi angka 0
         while (N > 0) {
-        	enablePrint = N % 10;
-        	if (enablePrint == 0 && N % 10 != 1 && zero == 1) {
-                System.out.print("0");
+        	enablePrint = N % 10; //untuk print angka terbalik
+        	if (enablePrint == 0 && N % 10 != 1 && zero == 1) { //jika sudah ada angka selain 0 maka bisa print 0
+                System.out.print("0"); //print angka 0
             }
             if (enablePrint > 0) {
                 System.out.print(enablePrint);
-                zero = 1;
+                zero = 1; //memberitahu jika sudah ada angka selain 0
             }
             N /= 10;
         }
