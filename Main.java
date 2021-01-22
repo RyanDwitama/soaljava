4b071f45a3848d4dcc3fa8be21119fe5c5e21ab8
RyanDwitama <76994573+RyanDwitama@users.noreply.github.com>
2021-01-22 08:51:21 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+package github;
+
+public class Main {
+    public static void main(String[] args) {
+        solution(11011101);
+        solution(-100);
+        solution(521455130);
+        solution(7451000);
+    }
+
+    static void solution(int N) {
+        int enablePrint = 0;
+        int zero = 1;
+        while (N > 0) {
+        	enablePrint = N % 10;
+            if (enablePrint == 0 && zero == 0) {
+                System.out.print("0");
+            }
+            if(enablePrint > 0) {
+                System.out.print(enablePrint); 
+                zero = 0;
+            }
+            N /= 10;
+        }
+        System.out.println();
+    }
+}
