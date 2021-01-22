f6f6f1ec872e88ca73aff4c0de66d0074d5c913c
RyanDwitama <76994573+RyanDwitama@users.noreply.github.com>
2021-01-22 09:32:36 +0700

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(String S) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < S.length(); i++) {
+            switch (S.charAt(i)) {
+                case '(':
+                    stack.push(S.charAt(i));
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.peek() != '(') return 0;
+                    else stack.pop();
+                    break;
+            }
+        }
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
