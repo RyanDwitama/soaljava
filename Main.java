d785bcc5eadaa1d6bbb757ddfd812b71f30eb2bb
RyanDwitama <76994573+RyanDwitama@users.noreply.github.com>
2021-01-22 09:04:47 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,27 +1,26 @@
-package github;
-
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        solution(11011101);
-        solution(-100);
-        solution(521455130);
-        solution(7451000);
-    }
 
-    static void solution(int N) {
-        int enablePrint = 0;
-        int zero = 1;
-        while (N > 0) {
-        	enablePrint = N % 10;
-            if (enablePrint == 0 && zero == 0) {
-                System.out.print("0");
-            }
-            if(enablePrint > 0) {
-                System.out.print(enablePrint); 
-                zero = 0;
-            }
-            N /= 10;
-        }
-        System.out.println();
-    }
+	public static void main(String[] args) {
+		String S = "John";
+		class Solution {
+		    public int solution(String S) {
+		        Stack<Character> stack = new Stack<Character>();
+		        for (int i = 0; i < S.length(); i++) {
+		            switch (S.charAt(i)) {
+		                case '(':
+		                    stack.push(S.charAt(i));
+		                    break;
+		                case ')':
+		                    if (stack.isEmpty() || stack.peek() != '(') return 0;
+		                    else stack.pop();
+		                    break;
+		            }
+		        }
+		        return stack.isEmpty() ? 1 : 0;
+		    }
+		}
+
+	}
+
 }
