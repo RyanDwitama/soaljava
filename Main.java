d202d0de010366b22c6cad51b3028f81b5d6ac96
RyanDwitama <76994573+RyanDwitama@users.noreply.github.com>
2021-01-22 09:31:15 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,25 @@
-import java.util.*;
 public class Main {
+    public static void main(String[] args) {
+        solution(11011101);
+        solution(-100);
+        solution(521455130);
+        solution(7451000);
+    }
 
-	public static void main(String[] args) {
-		String S = "John";
-		class Solution {
-		    public int solution(String S) {
-		        Stack<Character> stack = new Stack<Character>();
-		        for (int i = 0; i < S.length(); i++) {
-		            switch (S.charAt(i)) {
-		                case '(':
-		                    stack.push(S.charAt(i));
-		                    break;
-		                case ')':
-		                    if (stack.isEmpty() || stack.peek() != '(') return 0;
-		                    else stack.pop();
-		                    break;
-		            }
-		        }
-		        return stack.isEmpty() ? 1 : 0;
-		    }
-		}
-
-	}
-
+    static void solution(int N) {
+        int enablePrint = 0;
+        int zero = 0;
+        while (N > 0) {
+        	enablePrint = N % 10;
+        	if (enablePrint == 0 && N % 10 != 1 && zero == 1) {
+                System.out.print("0");
+            }
+            if (enablePrint > 0) {
+                System.out.print(enablePrint);
+                zero = 1;
+            }
+            N /= 10;
+        }
+        System.out.println();
+    }
 }
