e4a3d65c2ae34ceffbebd0c07f0eb2480fe0bf04
RyanDwitama <76994573+RyanDwitama@users.noreply.github.com>
2021-01-22 09:41:57 +0700

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -6,14 +6,14 @@ class Solution {
         for (int i = 0; i < S.length(); i++) {
             switch (S.charAt(i)) {
                 case '(':
-                    stack.push(S.charAt(i));
-                    break;
+                    stack.push(S.charAt(i)); //untuk memasukan character pada string kedalam stack
+                    break; //break case
                 case ')':
-                    if (stack.isEmpty() || stack.peek() != '(') return 0;
-                    else stack.pop();
-                    break;
+                    if (stack.isEmpty() || stack.peek() != '(') return 0; //mengecheck stack dengan character. Jika stack tidak nested, maka return 0
+                    else stack.pop(); //jika stack nested, maka melanjutkan looping
+                    break; //break case
             }
         }
-        return stack.isEmpty() ? 1 : 0;
+        return stack.isEmpty() ? 1 : 0; //jika stack kosong (properly nested), maka return 1. Jika tidak maka return 0 
     }
 }
